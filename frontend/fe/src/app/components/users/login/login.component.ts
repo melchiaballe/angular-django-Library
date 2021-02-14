b7478d05c4972793cb8184cb5d92d32066e9f362
Melchi <melchiaballe@gmail.com>
2021-02-15 00:04:34 +0800

    frontend login changes

--- frontend/fe/src/app/components/users/login/login.component.ts
+++ frontend/fe/src/app/components/users/login/login.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Login } from '../../../commons/models/login.models';
+import { LoginForm } from '../../../commons/forms/login.forms';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  form : LoginForm;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.form = new LoginForm(new Login);
   }
 
+  onSubmit({ value, valid }: { value: Login, valid: boolean }) {
+    // send the form data to the backend if the value
+    // format are valid.
+    if (valid) {
+      alert("VALID")
+    }
+  }
 }
