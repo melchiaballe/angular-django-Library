7da24d3a6a63d1b24097b099e54e75500cf5b59c
Melchi Aballe <melchiaballe@gmail.com>
2021-02-17 23:26:52 +0800

    Changes for books model and dashboard template

--- books/models.py
+++ books/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from users.models import User
 
 # Create your models here.
+class Book(models.Model):
+    AVAILABLE = 'available'
+    CHECKED_OUT = 'checked out'
+    DAMAGED = 'damaged'
+    LOST = 'lost'
+    DIGITAL_COPY = 'digital copy'
+
+    STATUS_CHOICES = (
+        (AVAILABLE, 'Available'),
+        (CHECKED_OUT, 'Checked Out'),
+        (DAMAGED, 'Damaged'),
+        (LOST, 'Lost'),
+        (DIGITAL_COPY, 'Digital Copy'),
+    )
+    title = models.CharField(max_length=255, blank=True)
+    author = models.CharField(max_length=255, blank=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    location = models.CharField(max_length=255, blank=True)
+    status = models.CharField(default=AVAILABLE, max_length=255, choices=STATUS_CHOICES)
+
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+
+class Checkout(models.Model):
+
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    checked_out_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    checked_out_date = models.DateTimeField(auto_now_add=True)
+    returned_date = models.DateTimeField(blank=True)
+
+class Comments(models.Model):
+
+    message = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    is_deleted = models.BooleanField(default=False)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
